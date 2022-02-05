d4cfd22f2591515f129264a2e79a16471f4c82ed
SeoJW <wodnjs1000gk@naver.com>
2022-02-05 10:39:36 +0900

    입출력 연습문제 2557, 10718, 1000

---
+++ baekjoon_warmup/입출력/입출력_연습문제/입출력_연습문제/1000 A+B.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int num_input_1, num_input_2;
+//	int num_output;
+//
+//	cin >> num_input_1 >> num_input_2;
+//
+//	num_output = num_input_1 + num_input_2;
+//	cout << num_output;
+//}
---
+++ baekjoon_warmup/입출력/입출력_연습문제/입출력_연습문제/10718 We love kriii.cpp
@@ -0,0 +1,7 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cout << "강한친구 대한육군" << '\n' << "강한친구 대한육군";
+//}
